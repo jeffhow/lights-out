1ff5843691d0bf71e76dec3ab8cd23dad074d7f6
Jeff LeBoeuf <jeff@jeff.how>
2020-07-24 16:14:05 -0400

    responsive grid

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ function App() {
   return (
     <main className="App grid-container">
       <header className="App-header header">
-        <h1><span>Glow</span> <span>Stick</span></h1>
+        <h1><span>Lights</span> <span>Out</span></h1>
       </header>
       <section className='game-area'>
         <Game />
       </section>
-      <div className='left-ad'></div>
-      <div className='right-ad'></div>
       <footer className='footer'>
         <div>
           &copy;&nbsp;2020&nbsp;<a href="https://paladinwebgroup.com">Paladin Web Group <span className="brand"></span></a>
